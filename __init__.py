b3b5441ec20215ec0fb199a71d047fdd3a25065d
RAMIT NANDI <136625497+idnantimar@users.noreply.github.com>
2026-06-30 18:29:16 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,30 @@
+"""
+
+    [For Internal Use Only]
+
+    This folder contains large code blocks used throughout this project.
+
+    The modules are intended purely for code organization and modularity;
+    they are not designed as a general-purpose package. The functions are
+    highly specific to the project's workflow, but are documented sufficiently
+    to facilitate future modifications and maintenance.
+
+    Rather than cluttering the notebook with hundreds of lines of implementation
+    details, the code is kept separate so that the primary focus remains on
+    data analysis.
+
+    Thanks,
+    @idnantimar
+    
+"""
+
+
+from . import utils,preprocessing,ClassKeywordExtractor
+
+
+
+__all__ = [
+    "preprocessing",
+    "ClassKeywordExtractor",
+    "utils"
+]
